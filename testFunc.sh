3b1a3321d71ea0ab20b9c095426ebfbd87263d7a
Roger Smith <rsmith6559@aol.com>
2020-05-07 17:23:09 -0400

    experimented with arguments and scopes

--- testFunc.sh
+++ testFunc.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+
 function test()
 {
-	for word in "$@"
+	for arg in "$@"
 	do
-		echo "test.$word"
+		if [ -z $( echo $arg | egrep '=' ) ]
+		then
+			echo "$0:test.$arg"
+		else
+			kw=$( echo $arg | sed -E 's/^([[:alnum:]]+)\=[[:alnum:]]+$/\1/' )
+			val=$( echo $arg | sed -E 's/^[[:alnum:]]+\=([[:alnum:]]+)$/\1/' )
+			eval "$kw"="\"$val\""
+		fi
 	done
+	PROG="$0"
 }
 
-test cat dog ferret fish bird
+#test cat dog ferret fish bird
+test $@
+
+echo $PROG
+echo "roger == $roger"
 
 exit 0
 
